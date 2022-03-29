6629375b5cadf0ff4c2bc206eeb3d4563ae101d0
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-29 12:59:50 -0500

    add blog post model

---
+++ models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require('./connection')
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  date: Date,
+  imageLink: String,
+  excerpt: String,
+  categories: [String],
+  postBody: String 
+})
+
+module.exports = mongoose.model('Post', postSchema)
